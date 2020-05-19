22ebc8ccee35de704f6018b7ccd9f2ed3a410b80
Vivi Poit <vwpoit@gmail.com>
2020-05-19 17:17:58 +0630

    Create Item with Rails scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :items
   namespace :home do
     get :index
   end
---
+++ spec/views/items/edit.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "items/edit", type: :view do
+  before(:each) do
+    @item = assign(:item, Item.create!(
+      name: "MyString",
+      price: 1
+    ))
+  end
+
+  it "renders the edit item form" do
+    render
+
+    assert_select "form[action=?][method=?]", item_path(@item), "post" do
+
+      assert_select "input[name=?]", "item[name]"
+
+      assert_select "input[name=?]", "item[price]"
+    end
+  end
+end
